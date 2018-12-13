eb995b232e7c88a6684d950cc1e19e753acd97af
Marcos Carbonell <Marcos Carbonell>
2018-12-13 19:55:10 +0100

    finished blatt 08 ipk

---
+++ IPK_Programmierkurs/Ubungsblatt08/Aufgabe3/build/CMakeFiles/pointstest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointstest.dir/test.cc.obj"
+  "pointstest.pdb"
+  "pointstest.exe"
+  "pointstest.exe.manifest"
+  "libpointstest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointstest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ IPK_Programmierkurs/Ubungsblatt08/Aufgabe3/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/markc/OneDrive/Aufgaben_uni/programmier_aufgaben_repo/IPK_Programmierkurs/Ubungsblatt08/Aufgabe3
+# Build directory: C:/Users/markc/OneDrive/Aufgaben_uni/programmier_aufgaben_repo/IPK_Programmierkurs/Ubungsblatt08/Aufgabe3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_this_points "C:/Users/markc/OneDrive/Aufgaben_uni/programmier_aufgaben_repo/IPK_Programmierkurs/Ubungsblatt08/Aufgabe3/build/pointstest.exe")
